3b2ed2e7c0bfaf61038513127a2b885043790042
mfroeh <mfroeh0@pm.me>
2022-12-09 10:38:24 +0100

    Add more to lab2

--- robot_agent/src/tasks/task_communicate.c
+++ robot_agent/src/tasks/task_communicate.c
@@ -76,9 +76,6 @@ void task_communicate(void)
     // Assume that robot clock is the same as mission control clock
     // Has to start EXACTLY on the slot
 
-    printf("Send start\n");
-
-
     // Send queue that will be filled with victims
     queue_t* victims = queue_init();
     queue_t* maps = queue_init();
@@ -88,21 +85,22 @@ void task_communicate(void)
     // How many items of the specific type were in the send_list
     static int found[5];
 
-    /* --- Send Data --- */
+    // Traverse send list
     while(g_list_send->count != 0)
     {
       seq++;
-      found[g_list_send->last->data_type]++;
 
       queue_t* queue;
       // Allocate memory for data structure
-      switch(g_list_send->last->data_type)
+      switch(g_list_send->first->data_type)
       {
         // Robot pose
         case s_DATA_STRUCT_TYPE_ROBOT :
           data = (void *)malloc(sizeof(robot_t));
           queue = positions;
           printf("Got robot position\n");
+          // TODO: The last position should be sufficient
+          queue_empty(positions);
           break;
           // Victim information
         case s_DATA_STRUCT_TYPE_VICTIM :
@@ -115,6 +113,11 @@ void task_communicate(void)
           data = (void *)malloc(sizeof(pheromone_map_sector_t));
           queue = maps;
           printf("Got pheromone map\n");
+          // Keep only last 8 messages
+          if (found[s_DATA_STUCT_TYPE_PHEROMONE] % 8 == 0) {
+            printf("Clearing pheromones\n");
+            queue_empty(maps);
+          }
           break;
           // Command (for future use)
         case s_DATA_STRUCT_TYPE_CMD :
@@ -132,18 +135,52 @@ void task_communicate(void)
           break;
       }
 
-      // Get data from the list
-      doublylinkedlist_remove(g_list_send, g_list_send->last, data, &data_type);
-      queue_enqueue(queue, data, data_type);
+      // Increment found count
+      found[g_list_send->first->data_type]++;
 
+      // Get data from the list and push to chosen queue
+      doublylinkedlist_remove(g_list_send, g_list_send->first, data, &data_type);
+      queue_enqueue(queue, data, data_type);
     }
 
-    printf("\n1");
+    printf("Sending start\n");
 
+    // TODO: Free data
+    
+    // How many bytes we can still send 
+    int bytes_left = 153600 / 8 / 8 // bits per second / timeslot / byte count
+                                    
+    static int sent[5];
 
-    data = (void*)malloc(sizeof(robot_t));
-    queue_dequeue(positions, data, &data_type);
-    if (data != NULL) {
+    // Send victim messages
+    for (int i = 0; i < found[s_DATA_STRUCT_TYPE_VICTIM]; ++i) {
+      data = (void*)malloc(sizeof(victim_t));
+      queue_dequeue(victims, data, &data_type);
+
+      // Encode data into UDP packet
+      protocol_encode(udp_packet,
+          &udp_packet_len,
+          s_PROTOCOL_ADDR_BROADCAST,
+          g_config.robot_id,
+          g_config.robot_team,
+          s_PROTOCOL_TYPE_DATA,
+          seq,
+          g_message_sequence_id,
+          last_id,
+          data_type,
+          data);
+
+      if ((bytes_left -= udp_packet_len) >= 0) {
+        udp_broadcast(g_udps, udp_packet, udp_packet_len);
+        sent[data_type]++;
+      }
+    }
+
+    // Send position
+    for (int i = 0; i < 1; ++i) {
+      data = (void*)malloc(sizeof(robot_t));
+      queue_dequeue(positions, data, &data_type);
+      
       // Encode data into UDP packet
       protocol_encode(udp_packet,
           &udp_packet_len,
@@ -158,110 +195,63 @@ void task_communicate(void)
           data);
 
       // Broadcast packet
-      udp_broadcast(g_udps, udp_packet, udp_packet_len);
+      if ((bytes_left -= udp_packet_len) >= 0) {
+        udp_broadcast(g_udps, udp_packet, udp_packet_len);
+        sent[data_type]++;
+      }
     }
 
-    // free(data);
+    // Send pheromones
+    for (int i = 0; i < 8; ++i) {
+      data = (void*)malloc(sizeof(pheromone_map_sector_t));
+      queue_dequeue(maps, data, &data_type);
+      
+      // Encode data into UDP packet
+      protocol_encode(udp_packet,
+          &udp_packet_len,
+          s_PROTOCOL_ADDR_BROADCAST,
+          g_config.robot_id,
+          g_config.robot_team,
+          s_PROTOCOL_TYPE_DATA,
+          seq,
+          g_message_sequence_id,
+          last_id,
+          data_type,
+          data);
 
+      // Broadcast packet
+      if ((bytes_left -= udp_packet_len) >= 0) {
+        udp_broadcast(g_udps, udp_packet, udp_packet_len);
+        sent[data_type]++;
+      }
+    }
 
-    // Send all the victim information 
+    // Send stream data if possible
+    for (int i = 0; i < found[s_DATA_STRUCT_TYPE_STREAM]; ++i) {
+      data = (void*)malloc(sizeof(stream_t));
+      queue_dequeue(streams, data, &data_type);
+      
+      // Encode data into UDP packet
+      protocol_encode(udp_packet,
+          &udp_packet_len,
+          s_PROTOCOL_ADDR_BROADCAST,
+          g_config.robot_id,
+          g_config.robot_team,
+          s_PROTOCOL_TYPE_DATA,
+          seq,
+          g_message_sequence_id,
+          last_id,
+          data_type,
+          data);
 
-    // Idea
-    /* if (g_list_send->count != 0) { */
+      // Broadcast packet
+      if ((bytes_left -= udp_packet_len) >= 0) {
+        udp_broadcast(g_udps, udp_packet, udp_packet_len);
+        sent[data_type]++;
+      }
+    }
 
-    /*   printf("1"); */
-
-    /*   /1* #define s_DATA_STRUCT_TYPE_ROBOT		0 *1/ */
-    /*   /1* #define s_DATA_STRUCT_TYPE_VICTIM		1 *1/ */
-    /*   /1* #define s_DATA_STRUCT_TYPE_PHEROMONE	2 *1/ */
-    /*   /1* #define s_DATA_STRUCT_TYPE_CMD			3 *1/ */
-    /*   /1* #define s_DATA_STRUCT_TYPE_STREAM		4 *1/ */
-
-    /*   // Traverse send list in reverse */
-    /*   doublylinkedlist_node_t* node = g_list_send->last; */
-
-    /*   printf("2"); */
-    /*   while(node != NULL) */
-    /*   { */
-    /*     seq++; */
-    /*     printf("3"); */
-    /*     switch (node->data_type) { */
-    /*       case s_DATA_STRUCT_TYPE_VICTIM: */
-    /*         queue_enqueue(queue, node->data, node->data_type); */
-    /*         break; */
-    /*       case s_DATA_STRUCT_TYPE_ROBOT: */
-    /*         if (map == NULL) pos = node->data; */
-    /*         break; */
-    /*       case s_DATA_STRUCT_TYPE_PHEROMONE: */
-    /*         if (map == NULL) map = node->data; */
-    /*         break; */
-    /*       default: */
-    /*         printf("Got unhandled data type %d", node->data_type); */
-    /*         break; */
-    /*     } */
-
-    /*     kjkjk */
-    /*       printf("4"); */
-    /*     printf("5"); */
-    /*     node = node->prev; */
-    /*   } */
-
-    /*   // Empty the send list for reuse */
-    /*   doublylinkedlist_empty(g_list_send); */
-    /*   printf("6"); */
-
-    /*   // Append position and map to queue */
-    /*   queue_enqueue(queue, pos, s_DATA_STRUCT_TYPE_ROBOT); */
-    /*   queue_enqueue(queue, map, s_DATA_STRUCT_TYPE_PHEROMONE); */
-    /*   printf("6"); */
-
-    /*   // TODO */
-    /*   /1* Data Structure types *1/ */
-
-
-    /*   // TODO: Fill up the rest with stream data */
-
-    /* } */
-    /* /1* int bytes_sent = 0; *1/ */
-
-    /* /1* // How many items we sent per data type *1/ */
-    /* /1* static int sent[5]; *1/ */
-
-    /* /1* // Send items in send queue *1/ */
-    /* /1* queue_dequeue(queue, data, &data_type); *1/ */
-    /* /1* while (data != NULL) { *1/ */
-    /* /1*   // Encode data into UDP packet *1/ */
-    /* /1*   protocol_encode(udp_packet, *1/ */
-    /* /1*       &udp_packet_len, *1/ */
-    /* /1*       s_PROTOCOL_ADDR_BROADCAST, *1/ */
-    /* /1*       g_config.robot_id, *1/ */
-    /* /1*       g_config.robot_team, *1/ */
-    /* /1*       s_PROTOCOL_TYPE_DATA, *1/ */
-    /* /1*       seq, *1/ */
-    /* /1*       g_message_sequence_id, *1/ */
-    /* /1*       last_id, *1/ */
-    /* /1*       data_type, *1/ */
-    /* /1*       data); *1/ */
-
-    /* /1*   // Broadcast packet *1/ */
-    /* /1*   udp_broadcast(g_udps, udp_packet, udp_packet_len); *1/ */
-
-    /* /1*   // TODO: Free the sent item *1/ */
-    /* /1*   /2* free(data); *2/ *1/ */
-    /* /1*   data = null; *1/ */
-
-    /* /1*   bytes_sent += udp_packet_len; *1/ */
-    /* /1*   sent[data_type]++; *1/ */
-
-    /* /1*   printf("Sent another packet of type %d! Now sent %d bytes in this task execution.", data_type, bytes_sent); *1/ */
-
-    /* /1*   // Get next item from queue *1/ */
-    /* /1*   queue_dequeue(queue, data, &data_type); *1/ */
-    /* /1* } *1/ */
-
-    /* /1* // Free the queue *1/ */
-    /* /1* queue_destroy(queue); *1/ */
-    /* /1* } *1/ */
+    printf("Sending done\n");
 
     /* --- Receive Data --- */
     // Receive packets, decode and forward to proper process
